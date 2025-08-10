160bd69f3f310eeb381e187a9456c74b2033a8a7
Jun Ishikawa <hamnyan@outlook.com>
2025-08-10 16:53:23 +0900

    #11 Change code to use plugin-common library

---
+++ src/plugin/ecpg/parser/scanner/ECPGKeywords.java
@@ -0,0 +1,25 @@
+package plugin.ecpg.parser.scanner;
+
+import org.eclipse.cdt.core.model.ICLanguageKeywords;
+
+public class ECPGKeywords implements ICLanguageKeywords {
+
+	@Override
+	public String[] getKeywords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getBuiltinTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getPreprocessorKeywords() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/plugin/ecpg/parser/scanner/ECPGScanner.java
+++ src/plugin/ecpg/parser/scanner/ECPGScanner.java
@@ -1,8 +1,5 @@
 package plugin.ecpg.parser.scanner;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 import org.eclipse.cdt.core.dom.parser.IScannerExtensionConfiguration;
 import org.eclipse.cdt.core.parser.EndOfFileException;
 import org.eclipse.cdt.core.parser.FileContent;
@@ -11,17 +8,15 @@ import org.eclipse.cdt.core.parser.IScannerInfo;
 import org.eclipse.cdt.core.parser.IToken;
 import org.eclipse.cdt.core.parser.IncludeFileContentProvider;
 import org.eclipse.cdt.core.parser.ParserLanguage;
-import org.eclipse.cdt.internal.core.parser.scanner.CPreprocessor;
-import org.eclipse.cdt.internal.core.parser.scanner.TokenUtil;
-import org.eclipse.cdt.internal.core.parser.scanner.TokenWithImage;
+
+import plugin.common.parser.scanner.OverrideToken;
+import plugin.common.parser.scanner.PluginScanner;
+import plugin.common.parser.scanner.PluginTokenUtil;
 
 @SuppressWarnings("restriction")
-public class ECPGScanner extends CPreprocessor {
+public class ECPGScanner extends PluginScanner {
 
-	/*
-	 * Inner Queue for token manipulation.
-	 */
-	private Queue<IToken> tokens = new ArrayDeque<>();
+	private boolean isSqlcaProcessed = false;
 
 	public ECPGScanner(FileContent fileContent, IScannerInfo info, ParserLanguage language, IParserLogService log,
 			IScannerExtensionConfiguration configuration, IncludeFileContentProvider readerFactory) {
@@ -30,108 +25,95 @@ public class ECPGScanner extends CPreprocessor {
 
 	@Override
 	public IToken nextToken() throws EndOfFileException {
-		if (tokens.peek() != null) {
-			return tokens.poll();
+		if (tokens().peek() != null) {
+			return clearPrefetchedToken(tokens().poll());
 		}
 		IToken nextToken = super.nextToken();
-
-		// Ignore from "EXEC" to ";" tokens.
+		// Process "EXEC" to ";" tokens().
 		if ("exec".equalsIgnoreCase(nextToken.toString())) {
-			IToken peekNextToken;
+			if (!isSqlcaProcessed) {
+				OverrideToken dummyToken = new OverrideToken();
+				dummyToken.setOffset(-1);
+				dummyToken.setEndOffset(0);
+				PluginTokenUtil.addSqlcaTokensEcpg(this, dummyToken);
+				// Initialize sqlca struct.
+				tokens().add(PluginTokenUtil.createStructToken(this, dummyToken));
+				tokens().add(PluginTokenUtil.createIdentifierToken(this, dummyToken, "sqlca"));
+				tokens().add(PluginTokenUtil.createIdentifierToken(this, dummyToken, "sqlca"));
+				isSqlcaProcessed = true;
+			}
 			while (true) {
 				nextToken = super.nextToken();
 				if ("call".equalsIgnoreCase(nextToken.toString())) {
 					return super.nextToken();
 				}
 				if ("do".equalsIgnoreCase(nextToken.toString())) {
-					peekNextToken = super.nextToken();
-					nextToken.setNext(null);
-					if ("break".equalsIgnoreCase(peekNextToken.toString())
-							|| "continue".equalsIgnoreCase(peekNextToken.toString())) {
-						// ignore those tokens
-						continue;
-					}
-					return peekNextToken;
-				}
-				if (":".equalsIgnoreCase(nextToken.toString())) {
 					nextToken = super.nextToken();
-					// To prevent warning, assign dummy value.
-					IToken t1 = new TokenWithImage(IToken.tASSIGN, this, nextToken.getOffset(),
-							nextToken.getEndOffset(), TokenUtil.getImage(IToken.tASSIGN));
-					IToken t2 = new TokenWithImage(IToken.tSTRING, this, nextToken.getOffset(),
-							nextToken.getEndOffset(), "\"FAKE_STRING\"".toCharArray());
-					peekNextToken = super.nextToken();
-					nextToken.setNext(null);
-					if (!";".equals(peekNextToken.toString())) {
-						IToken t3 = new TokenWithImage(IToken.tCOMMA, this, nextToken.getOffset(),
-								nextToken.getEndOffset(), TokenUtil.getImage(IToken.tCOMMA));
-						tokens.add(nextToken);
-						tokens.add(t1);
-						tokens.add(t2);
-						tokens.add(t3);
+					if ("break".equalsIgnoreCase(nextToken.toString())
+							|| "continue".equalsIgnoreCase(nextToken.toString())) {
+						// ignore those tokens().
 						continue;
-					} else {
-						tokens.add(nextToken);
-						tokens.add(t1);
-						tokens.add(t2);
-						tokens.add(peekNextToken);
-						return tokens.poll();
 					}
+					return nextToken;
+				}
+				if (":".equals(nextToken.toString())) {
+					nextToken = super.nextToken();
+					// Modify tokens to be treated as variables.
+					tokens().add(nextToken);
+					tokens().add(PluginTokenUtil.createAssignToken(this, nextToken));
+					tokens().add(PluginTokenUtil.createIntegerToken(this, nextToken, "0"));
+					tokens().add(PluginTokenUtil.createSemiToken(this, nextToken));
+					continue;
 				}
 				if (";".equals(nextToken.toString())) {
-					peekNextToken = super.nextToken();
-					nextToken.setNext(null);
-					if (!"exec".equalsIgnoreCase(peekNextToken.toString())) {
-						tokens.add(peekNextToken);
-						return nextToken;
-					}
+					tokens().add(nextToken);
+					return clearPrefetchedToken(tokens().poll());
 				}
 			}
 		}
 
-		// sqlca
-		if ("sqlca".equalsIgnoreCase(nextToken.toString())) {
-			//
-			nextToken = super.nextToken();
-			if (!".".equals(nextToken.toString())) {
-				return nextToken;
-			}
-			//
-			nextToken = super.nextToken();
-			if ("sqlcaid".equalsIgnoreCase(nextToken.toString())) {
-				// char sqlcaid[8];
-				nextToken.setType(IToken.tINTEGER);
-			} else if ("sqlabc".equalsIgnoreCase(nextToken.toString())) {
-				// long sqlabc;
-				nextToken.setType(IToken.tINTEGER);
-			} else if ("sqlcode".equalsIgnoreCase(nextToken.toString())) {
-				// long sqlcode;
-				nextToken.setType(IToken.tINTEGER);
-			} else if ("sqlerrm".equalsIgnoreCase(nextToken.toString())) {
-				// sqlerrm
-				nextToken = super.nextToken();
-				if (".".equals(nextToken.toString())) {
-					nextToken = super.nextToken();
-					if ("sqlerrml".equalsIgnoreCase(nextToken.toString())
-							|| "sqlerrmc".equalsIgnoreCase(nextToken.toString())) {
-						// int sqlerrml; or char sqlerrmc[SQLERRMC_LEN];
-						nextToken.setType(IToken.tINTEGER);
-					}
-				}
-			} else if ("sqlerrp".equalsIgnoreCase(nextToken.toString())) {
-				// char sqlerrp[8];
-				nextToken.setType(IToken.tINTEGER);
-			} else if ("sqlerrd".equalsIgnoreCase(nextToken.toString())) {
-				// long sqlerrd[6];
-				nextToken.setType(IToken.tINTEGER);
-			} else if ("sqlwarn".equalsIgnoreCase(nextToken.toString())) {
-				// char sqlwarn[8];
-				nextToken.setType(IToken.tINTEGER);
-			} else if ("sqlstate".equalsIgnoreCase(nextToken.toString())) {
-				// char sqlstate[5];
-				nextToken.setType(IToken.tINTEGER);
-			}
-		}
+		// Process ECPG specific sqlca
+//		if ("sqlca".equalsIgnoreCase(nextToken.toString())) {
+//			//
+//			nextToken = super.nextToken();
+//			if (!".".equals(nextToken.toString())) {
+//				return nextToken;
+//			}
+//			nextToken = super.nextToken();
+//			if ("sqlcaid".equalsIgnoreCase(nextToken.toString())) {
+//				// char sqlcaid[8];
+//				nextToken.setType(IToken.tINTEGER);
+//			} else if ("sqlabc".equalsIgnoreCase(nextToken.toString())) {
+//				// long sqlabc;
+//				nextToken.setType(IToken.tINTEGER);
+//			} else if ("sqlcode".equalsIgnoreCase(nextToken.toString())) {
+//				// long sqlcode;
+//				nextToken.setType(IToken.tINTEGER);
+//			} else if ("sqlerrm".equalsIgnoreCase(nextToken.toString())) {
+//				// sqlerrm
+//				nextToken = super.nextToken();
+//				if (".".equals(nextToken.toString())) {
+//					nextToken = super.nextToken();
+//					if ("sqlerrml".equalsIgnoreCase(nextToken.toString())
+//							|| "sqlerrmc".equalsIgnoreCase(nextToken.toString())) {
+//						// int sqlerrml; or char sqlerrmc[SQLERRMC_LEN];
+//						nextToken.setType(IToken.tINTEGER);
+//					}
+//				}
+//			} else if ("sqlerrp".equalsIgnoreCase(nextToken.toString())) {
+//				// char sqlerrp[8];
+//				nextToken.setType(IToken.tINTEGER);
+//			} else if ("sqlerrd".equalsIgnoreCase(nextToken.toString())) {
+//				// long sqlerrd[6];
+//				nextToken.setType(IToken.tINTEGER);
+//			} else if ("sqlwarn".equalsIgnoreCase(nextToken.toString())) {
+//				// char sqlwarn[8];
+//				nextToken.setType(IToken.tINTEGER);
+//			} else if ("sqlstate".equalsIgnoreCase(nextToken.toString())) {
+//				// char sqlstate[5];
+//				nextToken.setType(IToken.tINTEGER);
+//			}
+//		}
 		return nextToken;
 	}
 
